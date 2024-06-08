4e997dc3db249d187b8ca667844d439fa2007d87
azbagas <azharbagaskara1@gmail.com>
2024-06-08 13:59:18 +0700

    change: exclusive to false

--- consumers/consumeQuizResult.js
+++ consumers/consumeQuizResult.js
@@ -13,23 +13,26 @@ async function consumeQuizResult(amqp, callback, transporter, senderEmail) {
       durable: true,
     });
 
-    const q = await channel.assertQueue('', { exclusive: true });
-    
+    const q = await channel.assertQueue("", { exclusive: false });
+
     console.log(`[*] Waiting for messages in ${q.queue}. To exit press CTRL+C`);
 
     await channel.bindQueue(q.queue, exchange, routingKey);
 
-    channel.consume(q.queue, async (msg) => {
-      if (msg.content) {
-        console.log(" [x] %s: Message Accepted", msg.fields.routingKey);
-        try {
-          await callback(msg.content.toString(), transporter, senderEmail);
-        } catch (err) {
-          console.error("Error handling message:", err);
+    channel.consume(
+      q.queue,
+      async (msg) => {
+        if (msg.content) {
+          console.log(" [x] %s: Message Accepted", msg.fields.routingKey);
+          try {
+            await callback(msg.content.toString(), transporter, senderEmail);
+          } catch (err) {
+            console.error("Error handling message:", err);
+          }
         }
-      }
-    }, { noAck: true });
-
+      },
+      { noAck: true }
+    );
   } catch (error) {
     console.error("Error:", error);
     setTimeout(consumeQuizResult, 10000); // Retry after 10 seconds
